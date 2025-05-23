207de088e2bc2805c3176609221246041626b8b0
solanagithubraunit15 <raunitjaiswal510@gmail.com>
2025-05-23 11:14:59 +0000

    DepositAccount Struct done

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+
+use core::f32::consts::PI;
+
+use pinocchio::{account_info::AccountInfo, entrypoint, program_error::ProgramError, pubkey::Pubkey, ProgramResult}
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    match instruction_data.split_first() {
+        Some((Deposit::DISCRIMINATOR, data)) => Deposit::try_from((data, accounts))?.process(),
+        Some((Withdraw::DISCRIMINATOR, _)) => Withdraw::try_from(accounts)?.process(),
+        _ => Err(ProgramError::InvalidInstructionData)
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct DepositAccounts<'a> {
+    pub owner: &'a AccountInfo,
+    pub vault: &'a AccountInfo,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+ impl<'a> TryFrom<&'a [AccountInfo]> for DepositAccounts<'a> {
+   type Error = ProgramError;
+
+   fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
+       let [owner, vault, _] = accounts else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+       };
+
+       if !owner.is_signer() {
+          return Err(ProgramError::InvalidInstructionOwner);
+       }
+
+       if vault.owner().ne(&pinocchio_system::ID) {
+          return Err(ProgramError::InvalidInstructionOwner);
+       }
+
+       if vault.lamports().ne(&0) {
+          return Err(ProgramError::InvalidAccountData);
+       }
+
+       let (vault_key, _) = find_program_address(&[b"vault", owner.key()], &crate::ID);
+       if vault.key().ne(&vault_key) {
+          return Err(ProgramError::InvalidAccountOwner)
+       }
+     
+         Ok(Self { owner, vault})
+   }
 }
